d736e2a9bc8bf578319725403e9dad48fc9de962
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2022-07-23 17:23:44 +0530

    dd

---
+++ Procfile
@@ -0,0 +1,1 @@
+ web: build/install/ktors/bin/ktors
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
